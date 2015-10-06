b060dae256aa66e7a9f446bc502220957b7a97d4
drewCo2 <andrew.a.ritz@gmail.com>
2015-10-06 16:42:23 -0400

    add missing update

--- ProxyDiffInit-Example.R
+++ ProxyDiffInit-Example.R
@@ -1,1 +1,16 @@
-ProxyDif
+# Example script for initializing variables for the ProxyDiff.R script.
+# Create a script file "ProxyDiff.R" to create and set the required environment variables.
+# You may copy and paste the contents of this file to get started.
+
+
+# The list of host fragments that will be applied to the url fragment list.
+HostList<- c("http://www.proxyHost", "http://www.realHost")
+
+# The list of source url fragments that will be used.
+SourceList<-"MyUrls.txt"    
+
+# The max number of urls that will be used from each source.
+MaxUrls<-5
+
+# Each URL will be tried once.  we recommend that you leave this at 1.
+RequestCount<-1
